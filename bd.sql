8228b6e3e85364aabe139ec564b47596df7f9059
Isra <israel_2896@hotmail.com>
2021-02-08 16:22:58 -0600

    se agregó una vista 404 e imagen en la ruta images

---
+++ bd.sql
@@ -0,0 +1,8 @@
+create database bdejemplo;
+use bdejemplo
+create table productos(
+    id integer not null auto_increment,
+    nombre varchar(255),
+    precio decimal(5, 2),
+    primary key(id)
+);
