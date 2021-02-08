35953ded8802115518e30e8b71e976bc467a3b7f
Isra <israel_2896@hotmail.com>
2021-02-08 16:38:50 -0600

    Se realicaron cambios en la vista index y el app

--- app.js
+++ app.js
@@ -2,40 +2,27 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var productosRouter = require('./routes/productos');
 
 var app = express();
 
-// view engine setup
+// se configura la ruta para las vistas de ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configuracion las rutas index y la ruta productos
 app.use('/', indexRouter);
 app.use('/productos', productosRouter);
 
-// catch 404 and forward to error handler
+//si se redirecciona una ruta que no existe mandar un error de pagina
 app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(404).render('404', {titulo: 'Error pagina 404'});
 });
 
 module.exports = app;
